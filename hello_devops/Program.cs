2e08962ee603e0a2b5704894fa0c0cfe9cc0edde
tanachote-su <tanachote.su@jinovel.com>
2022-01-12 16:45:16 +0700

    first commit

---
+++ hello_devops/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Threading;
+
+namespace hello_devops
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            //Console.WriteLine("Hello World!");
+            //Console.WriteLine("");
+            //Console.WriteLine("Press anykey to exit.");
+            //Console.ReadLine();
+            int seq = 0;
+            while (true)
+            {
+                Console.WriteLine($"#{++seq} {DateTime.Now}");
+                Thread.Sleep(1000);
+            }
+        }
+    }
+}
